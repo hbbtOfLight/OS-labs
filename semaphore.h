243c54373f04d3b5246bbd0ec3ae49e5495cb899
hbbtOfLight <kataryna.kazinskaya@gmail.com>
2022-03-26 00:03:09 +0300

    add semaphore task

---
+++ semaphore.h
@@ -0,0 +1,54 @@
+//
+// Created by katari on 3/25/22.
+//
+
+#ifndef THREAD_LAB_SEMAPHORE_H
+#define THREAD_LAB_SEMAPHORE_H
+
+#include <mutex>
+#include <condition_variable>
+
+template<int Count = 1>
+class Semaphore {
+public:
+    Semaphore() = default;
+
+    Semaphore(int counter) : internal_counter_(std::min(Count, counter)) {}
+
+    const int& count() const { return internal_counter_; }
+
+    void release() {
+        std::unique_lock<std::mutex> u_lock(mtx_);
+        if (internal_counter_ < Count) {
+            ++internal_counter_;
+        }
+        free_.notify_one();
+    }
+
+    void acquire() {
+        std::unique_lock<std::mutex> u_lock(mtx_);
+        if (internal_counter_ > 0) {
+            --internal_counter_;
+        } else {
+            free_.wait(u_lock);
+        }
+    }
+
+    bool try_acquire() {
+        std::unique_lock<std::mutex> u_lock(mtx_);
+        if (internal_counter_ > 0) {
+            --internal_counter_;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private:
+    std::mutex mtx_;
+    std::condition_variable free_;
+    int internal_counter_ = Count;
+
+};
+
+#endif //THREAD_LAB_SEMAPHORE_H
